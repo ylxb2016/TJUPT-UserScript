59d5c116f8d397a4ca7d6cbd56741776f80c1560
tongyifan <565813476@qq.com>
2018-04-20 00:50:55 +0800

    exclude me in addcss.php

--- upload-image.user.js
+++ upload-image.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Upload image to sm.ms
 // @namespace    http://tampermonkey.net/
-// @version      1.5.20180419
+// @version      1.6.20180420
 // @description  上传图片到图床（附件服务无法继续使用的解决办法xD
 // @author       杯杯杯杯具@TJUPT
 // @match        https://tjupt.org/*
@@ -9,6 +9,7 @@
 // @exclude      https://*tjupt.org/invite.php*
 // @exclude      https://*tjupt.org/userdetails.php*
 // @exclude      https://*tjupt.org/usercp.php*
+// @exclude      https://*tjupt.org/addcss.php*
 // @updateURL    https://github.com/tongyifan/TJUPT-UserScript/raw/master/upload-image.user.js
 // ==/UserScript==
 
