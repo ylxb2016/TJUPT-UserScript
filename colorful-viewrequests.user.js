62c40d7ba6d6d5221f6a04f3d3b5d94137c252ea
Yifan Tong <tongyfan@gmail.com>
2018-12-07 17:43:36 +0800

    Update colorful-viewrequests.user.js

--- colorful-viewrequests.user.js
+++ colorful-viewrequests.user.js
@@ -1,23 +1,37 @@
 // ==UserScript==
 // @name         Colorful viewrequests!
-// @version      0.1
+// @version      0.2
 // @description  求种区上色
 // @author       杯杯杯杯具@TJUPT
-// @match        https://*tjupt.org/viewrequests.php*
+// @match        https://tjupt.org/viewrequests.php*
+// @match        https://www.tjupt.org/viewrequests.php*
 // @updateURL    https://github.com/tongyifan/TJUPT-UserScript/raw/master/colorful-viewrequests.user.js
 // ==/UserScript==
 
+
+function getParam(name) {
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+    var r = window.location.search.substr(1).match(reg);
+    if (r !== null) {
+        return unescape(r[2]);
+    }
+    return null;
+}
+
 (function () {
     'use strict';
-    var trs = $("table[class='torrents'] > tbody > tr");
-    for (var i = 0; i < trs.length; i++) {
-        if (trs.eq(i).children("td").size() < 5 || trs.eq(i).children("td").eq(6).text() === "应求") {
-            continue;
-        }
-        if (trs.eq(i).children("td").eq(6).text() === '0') {
-            trs.eq(i).css("background", "#ebf260");
-        } else {
-            trs.eq(i).css("background", "#87CEEB");
+    var action = getParam("action");
+    if (action === null || action === "list") {
+        var trs = $("table[class='torrents'] > tbody > tr");
+        for (var i = 0; i < trs.length; i++) {
+            if (trs.eq(i).children("td").size() < 5 || trs.eq(i).children("td").eq(6).text() === "应求") {
+                continue;
+            }
+            if (trs.eq(i).children("td").eq(6).text() === '0') {
+                trs.eq(i).css("background", "#ebf260");
+            } else {
+                trs.eq(i).css("background", "#87CEEB");
+            }
         }
     }
 })();
